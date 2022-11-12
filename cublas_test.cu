1672c66a543a22a7de7860f0ee8a2c20796c900d
maniben3 <106887188+maniben3@users.noreply.github.com>
2022-11-12 11:50:00 +0530

    Update cublas_test.cu

--- cublas_test.cu
+++ cublas_test.cu
@@ -1,29 +1,15 @@
-#include <cublas_v2.h>
-#include <stdio.h>    
-typedef float ftype;
-extern "C"
+#include <stdio.h>
+#include <assert.h>
+#include <cuda.h>
+#include <cuda_runtime.h>
 
-void run(ftype *i1, ftype  *i2, ftype *o1, int d);
+__global__ void test(){
+    printf("Hi Cuda World");
+}
 
-void run(ftype *i1, ftype  *i2, ftype *o1, int d){
-
-	ftype *d_i1, *d_i2, *d_o1;
-	int ds = d*d*sizeof(ftype);
-	cudaMalloc(&d_i1, ds);
-	cudaMalloc(&d_i2, ds);
-	cudaMalloc(&d_o1, ds);
-	cudaMemcpy(d_i1, i1, ds, cudaMemcpyHostToDevice);
-	cudaMemcpy(d_i2, i2, ds, cudaMemcpyHostToDevice);
-	
-        printf("super/n");
-	cublasHandle_t h;
-	cublasCreate(&h);
-	ftype alpha = 1.0;
-	ftype beta = 0.0;
-	cublasSgemm(h, CUBLAS_OP_N, CUBLAS_OP_N, d, d, d, &alpha, d_i1, d, d_i2, d, &beta, d_o1, d);
-	cudaMemcpy(o1, d_o1, ds, cudaMemcpyDeviceToHost);
-	
-	cudaFree(d_i1);
-	cudaFree(d_i2);
-	cudaFree(d_o1);
+int main( int argc, char** argv )
+{
+    test<<<1,1>>>();
+    cudaDeviceSynchronize();
+    return 0;
 }
