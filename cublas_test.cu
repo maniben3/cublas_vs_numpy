6ccd13555d01b6a0dd2e0d87ecc7eda259ad5d07
maniben3 <106887188+maniben3@users.noreply.github.com>
2022-11-12 12:25:47 +0530

    Update cublas_test.cu

--- cublas_test.cu
+++ cublas_test.cu
@@ -1,16 +1,8 @@
 #include <stdio.h>
-#include <assert.h>
-#include <cuda.h>
-#include <cuda_runtime.h>
 
-
-__global__ void test(){
-    printf("Hi Cuda World");
-}
 void myprint(void);
 
 void myprint()
 {
-    test<<<1,1>>>();
     printf("hello world\n");
 }
