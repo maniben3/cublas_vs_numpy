0d4c15ef2f09a23983600d8d754c70e49c2faa77
maniben3 <106887188+maniben3@users.noreply.github.com>
2022-11-12 15:17:33 +0530

    Update testlib.c

--- testlib.c
+++ testlib.c
@@ -1,6 +1,8 @@
-#include <stdio.h>
-
-int main(void) {
-   printf("Hello, World! \n");
-   return 0;
+int our_function(int num_numbers, int *numbers) {
+    int i;
+    int sum = 0;
+    for (i = 0; i < num_numbers; i++) {
+        sum += numbers[i];
+    }
+    return sum;
 }
