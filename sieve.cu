122b356022c5f4da5b5e473a8283e326b44d6e43
maniben3 <106887188+maniben3@users.noreply.github.com>
2022-11-12 15:19:17 +0530

    Update sieve.cu

--- sieve.cu
+++ sieve.cu
@@ -279,8 +279,6 @@ cleanup:
 
 int our_function(int num_numbers, int *numbers) {
     struct timeval tv1, tv2;
-    gettimeofday(&tv1, NULL);
-    sieve(num_numbers);
     gettimeofday(&tv2, NULL);
     printf("Total time = %f seconds\n", (double) (tv2.tv_usec - tv1.tv_usec) / 1000000 + (double) (tv2.tv_sec - tv1.tv_sec));
 }
